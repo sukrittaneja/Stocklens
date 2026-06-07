641767cd64e212ebffa4d3eee19391a9ceb5707f
sukrittaneja <sukrittaneja@gmail.com>
2026-06-08 01:30:04 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+// Unregister old service worker
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll().then(clients => 
+        clients.forEach(c => c.navigate(c.url))
+      ))
+  );
+});
